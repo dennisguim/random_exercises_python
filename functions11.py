7f15c109f3f144edb420e817d3faa3e81415f52e
dennisguim <drinaldim@gmail.com>
2025-04-08 10:11:58 -0300

    Update functions11.py

--- functions11.py
+++ functions11.py
@@ -1,2 +1,16 @@
 # 11. Check if a Number is Perfect
 # Número positivo
+
+
+
+def perfection():
+    num = int(input('Input a interger number to be checked: '))
+    soma = 0    
+    divisors = [n for n in range(1, num)
+                if num % n == 0
+                ]
+    for n in divisors:
+        soma += n
+    return "It is a perfect number." if soma == num else "It is not a perfect number."
+
+print(perfection())
